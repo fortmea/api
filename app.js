0c209470ad364cf1fbda977805868e28cf51688b
João Walter Amadeu <joaowa2002@live.com>
2021-03-10 20:23:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = express()
 //const port = 3000
 
 app.get('/', (req, res) => res.send("Hello World!"))
-app.listen(process.env.PORT,'127.0.0.1', () => console.log(`Application listening on port ${port}!`))
+app.listen(process.env.PORT, '0.0.0.0');
+console.log(`Application listening on port ${process.env.PORT}!`)
