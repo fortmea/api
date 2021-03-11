fc01cf96fdf4381c3d30132900fb9b2c95204267
João Walter Amadeu <joaowa2002@live.com>
2021-03-10 22:36:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
-app.use(express.urlencoded({extended: true}));
+//var qs = require('querystring');
+app.use(express.urlencoded({extended:true}));
 // default route
 app.get('/', function (req, res) {
 return res.send({ error: true, message: 'hello' })
@@ -16,38 +17,57 @@ var dbConn = mysql.createConnection({
 // connect to database
 dbConn.connect(); 
 // Retrieve all usuario 
-app.get('/usuario', function (req, res) {
+
+/*app.post('/usuario', function (req, res) {
 dbConn.query('SELECT * FROM usuario', function (error, results, fields) {
 if (error) throw error;
 return res.send({ error: false, data: results, message: 'usuario list.' });
 });
-});
+});*/
 // Retrieve usuario with id 
-app.get('/usuario/:id', function (req, res) {
-let usuario_id = req.params.id;
+app.use((req, res, next) => {
+    let validIps = ['::12', '127.0.0.1','::1','68.183.126.19']; // Put your IP whitelist in this array
+    
+      if(validIps.includes(req.socket.remoteAddress)){
+          // IP is ok, so go on
+          console.log("IP "+req.socket.remoteAddress+" ok");
+          next();
+      }
+      else{
+          // Invalid ip
+          console.log("Bad IP: " + req.connection.remoteAddress);
+          const err = new Error("Bad IP: " + req.connection.remoteAddress);
+          next(err);
+      }
+    })
+app.post('/usuario/', function (req, res) {
+let usuario_id = req.body.id;
 if (!usuario_id) {
 return res.status(400).send({ error: true, message: 'Please provide usuario_id' });
 }
 dbConn.query('SELECT * FROM usuario where id=?', usuario_id, function (error, results, fields) {
 if (error) throw error;
-return res.send({ error: false, data: results[0], message: 'usuario list.' });
+return res.send({ error: false, data: results[0], message: 'Ok.' });
 });
 });
 // Login
-app.get('/usuario/login/:nome&:senha', function (req, res) {
-    let usuario_nome = req.params.nome;
-    let usuario_senha = req.params.senha;
-    console.log(req.params);
-    if (!usuario_nome||!usuario_senha) {
+app.post('/login/', function (req, res) {
+    let usuario_nome = req.body.nome;
+    let usuario_senha = req.body.senha;
+    //console.log("Parametros: "+JSON.stringify(req.body)+"       ");
+    if ((!usuario_nome) || (!usuario_senha)) {
     return res.status(400).send({ error: true, message: 'Please provide name' });
     }
-    dbConn.query('SELECT * FROM usuario where nome=? and senha=?', usuario_id, usuario_senha, function (error, results, fields) {
+    var querys = "SELECT * FROM usuario where nome='"+usuario_nome+ "' and senha='"+usuario_senha+"' ";
+    //console.log(querys);
+    dbConn.query(querys, function (error, results, fields) {
     if (error) throw error;
-    return res.send({ error: false, data: results[0], message: 'usuario list.' });
+    return res.send({ error: false, data: results,message:"Ok."});
     });
     });
 // set port
 app.listen(process.env.PORT || 5000, function() {
     console.log("Server started.......");
 });
+
 module.exports = app;
