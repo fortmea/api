49b7e7b8e15b67c3aad89b2f7e711eb63e36ad59
João Walter Amadeu <joaowa2002@live.com>
2021-03-16 23:51:44 -0300

    Temporary redis removal

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var app = express();
 var mysql = require('mysql');
 var cors = require('cors')
 const crypto = require("crypto");
-const session = require('express-session');
+/*const session = require('express-session');
 const redis = require('redis');
 const redisStore = require('connect-redis')(session);
 const client  = redis.createClient();
-const router = express.Router();
+const router = express.Router();*/
 //var qs = require('querystring');
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
@@ -23,7 +23,7 @@ var dbConn = mysql.createConnection({
     database: 'db_web'
 });
 dbConn.connect(); 
-app.use(session({
+/*app.use(session({
     secret: 'zeD5UK8je13uqkTg8k7D8M7kU6EvqBrTMR38idXssPVZmvePixhdWmYV8DbOkbMqCMn3L2rCNZkG2DcvUTSupbuPBRZ5vqe2INDdiAh0ShfN5CeZrvZmE4L9HQGTgm1nCfKtCdQTtVLS8alaMd2iNVSykT4Em6KfcBcd3RizMDgLhuvD8soOMBrReyS9bzFAdhsIEV94p4kZfjVe42IfoxGp6OkVQOoMa4Y7I4k1sty5G77fdGP53ycgs3xiCG3oVNE0yKWySO6Z5snB9t9yEwqJUqurx6eDZkfpQxKyfwiNRfo7DTjMgn0Dqbz7ozmHyT7wcFY7bUIKmsQRruwI98bFfAAtlZvL0OL46EPcmBinz4XqYOURrUpnN2Rg4saRnwwr24CNKQzYP0xRWc1Zvl24MF0lDeUqnbC9VmMcoAPHUmStVCoOFtBEcGrVS1KWUJHnQXkbqDSU4b1VIXbpYWlK8R31grgOSuliDUpLvA4y8xKnO0aqdELumUOKsHOwd3lN7w5kw81yRt9qszsTIzadVfoqNw3rpvvt8WzwVUiRi5I6nZUTbsHhOnJdoeT1yHg9mgWEdW9AF7YYpWAQDBQiAduosNWXTTI3YxcwuF4l1x0t5HkRnijO2qarUZXJsFcNBm6WljyQ5y6jaB2OrERTnDC0wbwrlV6QRzUUJrrFCZamjBbCFIhrcQeJsgILuh3b58G1Y3JtKeduupnZZzdK4N9k4nDaeqef8mEb8E5FHAHolHhL8yr0GfrdwjJTi1y9dX1tkaS5zXVyQy4sG9kJCegP2AvsIZYnAXRdzoBxweTujatBN2TmfXq2k9s0P18mdJ0hIfaNLZ0SyOB2E9tQpBJhPn9nqakT2mLg3QnrDFOvIHWV3AhSJdlP2mQVSbBHwNHi01UseKlzvr2b5hyNLcjmKMp2zhQav6qp6SejYkZfgzKzi2DbISK9GwEJHa6V3tljMPXmmvPTRJxwGhGAgaszqWEAtUC2qiC7VSZ8NFHqbvDqlGAROULs1mulO4Z2zhYwX0jkTZTTZvz6t1bZoQ9xnYk0F5uSEnIHxctRihSqFwYnwjzWeDOrYrvmaMvjgERvhx2ezLEC26rdJiBEW8otyKeu0n76dsvR94Z8gawkL9Y3PwhoJOHeUAECLjCmhBNGw4ViBvdLK1yOcJwogws0CChbNgxJ9wAUe2P67cxe66uhpmTcusjW94l3cDTzU01aeFbVqPKNhQYSlg4m4uVCgQcYoG9ddjTk3D9N96U2Ifz9Dcy0mdrPt8HMIZEFbBWqF4qMx3mPoiS5UpuZZOHWk9GbXTzLVGpkzSsDM59JJzOVEqBzqZ5U4eeojFxjCAWhN2FlUmftwFAfVbklyVVchkjleIgdZ7i0FWLcuCWgecByhxMlYrdIjEmHQ89Fjm3HUScvPGQ3iFVOqh8fgJ8SVn1QFGOYibcs1bzcGaOMVrVBCNgzm1fvmMnYk9CHDkmN4XkH14shU74bqZrEQd8onHCEhOO5weawsIb06253MB3WXv9E64Q8I9u4DIQUIUaAAN4ABi2WddNc39B5G9FakGPzxP435QJadUFY68AAiYhVMFLdUsWsTaq3mla4v6Q66WOUsaioq9s4O9NpVPigTDo5dW0CF9tw194QYjKOuTa1IGBcbT096aPvRAFslq1WOMlOvqPLBKMjyx1m6ff5e3ij6CJDqsbNHGuaQmr9MpqlTAfRKYgecrGV5oentXnNGt5QeFFHWo8eYds0Dia4zqKSEiOpcGR9ah9Cwt2MtLCRmMAcaLy5ivUrymG7pZKQdeGgHp04oDbNz4RqC1RARGpZEa4D0BgjQJjRcu99x36JvVb4kI9NChzGvjOerhPDzyXk7LMWoCqBwBjTvebxsE06PbEeN8Fr3zmnNV67VfHXgz3A8W1G0LjZnLI3rJ7hVWZJbye1j7FohiSHiorft97M9NJ6AKGUgrYSksspGGQvybgd4vqrNdjeE5kSC7A7qkTC3tax',
     store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl : 260}),
     saveUninitialized: false,
@@ -43,7 +43,7 @@ app.use(session({
           next(err);
       }
     })*/
-router.post('/login',(req,res) => {
+app.post('/login',(req,res) => {
     let usuario_email = req.body.email;
     var usuario_nome;
     var usuario_timestamp;
@@ -70,7 +70,7 @@ router.post('/login',(req,res) => {
     }
     
 });
-router.post('/usuario/', function (req, res) {
+app.post('/usuario/', function (req, res) {
     console.log(req.body);
 let usuario_id = req.body.id;
 //console.log(usuario_id)
@@ -82,14 +82,14 @@ if (error) throw error;
 return res.send({ error: false, data: results[0], message: 'Ok.' });
 });
 });
-router.post('/post/', function (req, res) {
+app.post('/post/', function (req, res) {
     
     dbConn.query('SELECT * FROM post ORDER BY id DESC', function (error, results, fields) {
     if (error) throw error;
     return res.send({ error: false, data: results });
     });
     });
-router.post('/addpost/', function (req, res) {
+app.post('/addpost/', function (req, res) {
     let autor = req.body.id;
     let conteudo = req.body.conteudo;
     let nome = req.body.titulo;
@@ -107,7 +107,7 @@ router.post('/addpost/', function (req, res) {
     });
 });
 // Login
-router.post('/gerarhash/', function (req, res) {
+app.post('/gerarhash/', function (req, res) {
     let usuario_nome = req.body.nome;
     let usuario_email = req.body.email;
     var str = usuario_nome+usuario_email;
@@ -129,7 +129,7 @@ router.post('/gerarhash/', function (req, res) {
     });
     });
 
-router.post('/login/', function (req, res) {
+app.post('/login/', function (req, res) {
     let usuario_nome = req.body.nome;
     let usuario_senha = req.body.senha;
     //console.log("Parametros: "+JSON.stringify(req.body)+"       ");
@@ -144,7 +144,7 @@ router.post('/login/', function (req, res) {
     });
 });
 // set port
-app.use('/', router);
+//app.use('/', router);
 app.listen(process.env.PORT || 5000, function() {
     console.log("Server started.......");
 });
