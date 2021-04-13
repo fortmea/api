a2c9883a0f9089ce15aab2f3f57dd9d4cc979aab
João Walter Amadeu <joaowa2002@live.com>
2021-04-13 17:47:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var mysql = require('mysql');
 var cors = require('cors')
 const crypto = require("crypto");
+const secret = process.env.secret;
 /*const session = require('express-session');
 const redis = require('redis');
 const redisStore = require('connect-redis')(session);
@@ -24,26 +25,11 @@ var dbConn = mysql.createConnection({
 });
 dbConn.connect(); 
 /*app.use(session({
-    secret: 'zeD5UK8je13uqkTg8k7D8M7kU6EvqBrTMR38idXssPVZmvePixhdWmYV8DbOkbMqCMn3L2rCNZkG2DcvUTSupbuPBRZ5vqe2INDdiAh0ShfN5CeZrvZmE4L9HQGTgm1nCfKtCdQTtVLS8alaMd2iNVSykT4Em6KfcBcd3RizMDgLhuvD8soOMBrReyS9bzFAdhsIEV94p4kZfjVe42IfoxGp6OkVQOoMa4Y7I4k1sty5G77fdGP53ycgs3xiCG3oVNE0yKWySO6Z5snB9t9yEwqJUqurx6eDZkfpQxKyfwiNRfo7DTjMgn0Dqbz7ozmHyT7wcFY7bUIKmsQRruwI98bFfAAtlZvL0OL46EPcmBinz4XqYOURrUpnN2Rg4saRnwwr24CNKQzYP0xRWc1Zvl24MF0lDeUqnbC9VmMcoAPHUmStVCoOFtBEcGrVS1KWUJHnQXkbqDSU4b1VIXbpYWlK8R31grgOSuliDUpLvA4y8xKnO0aqdELumUOKsHOwd3lN7w5kw81yRt9qszsTIzadVfoqNw3rpvvt8WzwVUiRi5I6nZUTbsHhOnJdoeT1yHg9mgWEdW9AF7YYpWAQDBQiAduosNWXTTI3YxcwuF4l1x0t5HkRnijO2qarUZXJsFcNBm6WljyQ5y6jaB2OrERTnDC0wbwrlV6QRzUUJrrFCZamjBbCFIhrcQeJsgILuh3b58G1Y3JtKeduupnZZzdK4N9k4nDaeqef8mEb8E5FHAHolHhL8yr0GfrdwjJTi1y9dX1tkaS5zXVyQy4sG9kJCegP2AvsIZYnAXRdzoBxweTujatBN2TmfXq2k9s0P18mdJ0hIfaNLZ0SyOB2E9tQpBJhPn9nqakT2mLg3QnrDFOvIHWV3AhSJdlP2mQVSbBHwNHi01UseKlzvr2b5hyNLcjmKMp2zhQav6qp6SejYkZfgzKzi2DbISK9GwEJHa6V3tljMPXmmvPTRJxwGhGAgaszqWEAtUC2qiC7VSZ8NFHqbvDqlGAROULs1mulO4Z2zhYwX0jkTZTTZvz6t1bZoQ9xnYk0F5uSEnIHxctRihSqFwYnwjzWeDOrYrvmaMvjgERvhx2ezLEC26rdJiBEW8otyKeu0n76dsvR94Z8gawkL9Y3PwhoJOHeUAECLjCmhBNGw4ViBvdLK1yOcJwogws0CChbNgxJ9wAUe2P67cxe66uhpmTcusjW94l3cDTzU01aeFbVqPKNhQYSlg4m4uVCgQcYoG9ddjTk3D9N96U2Ifz9Dcy0mdrPt8HMIZEFbBWqF4qMx3mPoiS5UpuZZOHWk9GbXTzLVGpkzSsDM59JJzOVEqBzqZ5U4eeojFxjCAWhN2FlUmftwFAfVbklyVVchkjleIgdZ7i0FWLcuCWgecByhxMlYrdIjEmHQ89Fjm3HUScvPGQ3iFVOqh8fgJ8SVn1QFGOYibcs1bzcGaOMVrVBCNgzm1fvmMnYk9CHDkmN4XkH14shU74bqZrEQd8onHCEhOO5weawsIb06253MB3WXv9E64Q8I9u4DIQUIUaAAN4ABi2WddNc39B5G9FakGPzxP435QJadUFY68AAiYhVMFLdUsWsTaq3mla4v6Q66WOUsaioq9s4O9NpVPigTDo5dW0CF9tw194QYjKOuTa1IGBcbT096aPvRAFslq1WOMlOvqPLBKMjyx1m6ff5e3ij6CJDqsbNHGuaQmr9MpqlTAfRKYgecrGV5oentXnNGt5QeFFHWo8eYds0Dia4zqKSEiOpcGR9ah9Cwt2MtLCRmMAcaLy5ivUrymG7pZKQdeGgHp04oDbNz4RqC1RARGpZEa4D0BgjQJjRcu99x36JvVb4kI9NChzGvjOerhPDzyXk7LMWoCqBwBjTvebxsE06PbEeN8Fr3zmnNV67VfHXgz3A8W1G0LjZnLI3rJ7hVWZJbye1j7FohiSHiorft97M9NJ6AKGUgrYSksspGGQvybgd4vqrNdjeE5kSC7A7qkTC3tax',
+    secret:process.env.secret ,
     store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl : 260}),
     saveUninitialized: false,
     resave: false
 }));
- app.use((req, res, next) => {
-    let validIps = ['::12', '127.0.0.1','::1','68.183.126.19']; // Put your IP whitelist in this array
-      if(validIps.includes(req.socket.remoteAddress)){
-          // IP is ok, so go on
-          console.log("IP "+req.socket.remoteAddress+" ok");
-          next();
-      }
-      else{
-          // Invalid ip
-          console.log("Bad IP: " + req.socket.remoteAddress);
-          const err = new Error("Bad IP: " + req.socket.remoteAddress);
-          next(err);
-      }
-    })
-    
 app.post('/login/',(req,res) => {
     let usuario_email = req.body.email;
     let usuario_hash = req.body.hash;
@@ -61,7 +47,6 @@ app.post('/login/',(req,res) => {
         console.log(usuario_timestamp);
         var str = usuario_nome+usuario_email;
         var datb = usuario_timestamp+str;
-        const secret = "KNq72SajoZ2mNtzpBuCxo1ANOYKr7wllYAOzTL7fAZQgrwdHnl2gwizXShYQEBiB1QqC5sdsEkXum0jaWtIwcz57d1l9zGACI68HgPHwENbAdZejG1LlB3XdGyGJE7hEVNVAjF2ByiMoFExmDwQiITsFNPR78MKHXGPpmjPGVjtZ1ShrG3nZpkq7dWfDpmmriGHp0jJI";
         const md5Hasher = crypto.createHmac("md5", secret);
         const hash =  md5Hasher.update(datb).digest("hex");
         console.log(hash);
@@ -140,7 +125,6 @@ app.post('/addpost/', function (req, res) {
                 console.log(usuario_timestamp);
                 var str = usuario_nome+usuario_email;
                 var datb = usuario_timestamp+str;
-                const secret = process.env.secret;
                 const md5Hasher = crypto.createHmac("md5", secret);
                 const hash =  md5Hasher.update(datb).digest("hex");
                 if(usuario_confirmado==1){
@@ -192,7 +176,6 @@ app.post('/confirmar/',function(req,res){
                 console.log(usuario_timestamp);
                 var str = usuario_nome+usuario_email;
                 var datb = usuario_timestamp+str;
-                const secret = process.env.secret;
                 const md5Hasher = crypto.createHmac("md5", secret);
                 const hash =  md5Hasher.update(datb).digest("hex");
                 if(usuario_confirmado==0){
@@ -230,7 +213,6 @@ app.post('/register/', function (req, res) {
     var data = new Date();
     const time = data.getTime();
     var datb = time+str;
-    const secret = process.env.secret;
     const md5Hasher = crypto.createHmac("md5", secret);
     const hash =  md5Hasher.update(datb).digest("hex");
     if ((!usuario_nome)) {
