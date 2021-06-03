51c3885b9a722265002b806306e0b8768bef0940
João Walter Amadeu <joaowa2002@live.com>
2021-06-02 23:39:54 -0300

    Mudança nos requisitos de níveis de acesso

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ app.post('/addpost/', function (req, res) {
                 var usuario_confirmado = results[0].confirmado; //|insere as informações obtidas do Banco de Dados MySql
                 var usuario_level = results[0].level; //|
                 if (usuario_confirmado == 1) { //caso usuário seja confirmado:
-                    if (usuario_level != 1) { //caso o usuário não tenha permissão para postar:
+                    if (usuario_level < 1) { //caso o usuário não tenha permissão para postar:
                         return res.send({ error: 'true', data: "Usuário não tem permissão para fazer publicações!<br>Caso discorde disso, entre em contato em <a href='mailto:suporte@" + email_server + "'>suporte@" + email_server + "</a> ou <a href='mailto:" + admin + "@" + email_server + "'>" + admin + "@" + email_server + "</a>." });
                     } else { //caso tenha permissão para postar:
                         const today = new Date();
